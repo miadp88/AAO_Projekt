59de6c6097ab5afa7f87401322880f0f70390bff
miadp88 <54668407+miadp88@users.noreply.github.com>
2021-12-06 12:52:23 +0100

    filter and buttons over table

--- JaveScript/Layout/_global.js
+++ JaveScript/Layout/_global.js
@@ -50,3 +50,12 @@ for (i = 0; i < dropdown.length; i++) {
   }
   });
 }
+
+function myFunction() {
+  var x = document.getElementById("Demo");
+  if (x.className.indexOf("w3-show") == -1) { 
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
