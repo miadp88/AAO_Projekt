978c408e4a941567fee6dc92194177bed297c350
miadp88 <54668407+miadp88@users.noreply.github.com>
2021-12-07 11:00:51 +0100

    .

--- JaveScript/Layout/_global.js
+++ JaveScript/Layout/_global.js
@@ -36,31 +36,17 @@ $('[data-toggle="tooltip"]').tooltip();
 
 
 
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
 
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-  this.classList.toggle("active");
-  var dropdownContent = this.nextElementSibling;
-  if (dropdownContent.style.display === "block") {
-  dropdownContent.style.display = "none";
-  } else {
-  dropdownContent.style.display = "block";
-  }
-  });
-}
-
-function myFunction() {
+/*function myFunction() {
   var x = document.getElementById("Demo1");
   if (x.className.indexOf("w3-show") == -1) { 
     x.className += " w3-show";
   } else {
     x.className = x.className.replace(" w3-show", "");
   }
-}
+}*/
 
-function myFunction() {
+/*function myFunction() {
   var x = document.getElementById("Demo2");
   if (x.className.indexOf("w3-show") == -1) { 
     x.className += " w3-show";
@@ -68,3 +54,4 @@ function myFunction() {
     x.className = x.className.replace(" w3-show", "");
   }
 }
+*/
